7b3db66c0099cb75b756add73f20d3815e84d3b2
Rafael Kitover <rkitover@gmail.com>
2020-03-02 23:39:15 +0000

    Center clock in the terminal.
    Center the clock in the terminal using $LINES and $COLUMNS.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- clock.sh
+++ clock.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
-for req in tput toilet; do
-    if ! command -v $req >/dev/null; then
-        printf >&2 "ERROR: please install $req"
-        exit 1
-    fi
-done
+main() {
+    for req in tput toilet; do
+        if ! command -v $req >/dev/null; then
+            printf >&2 "ERROR: please install $req"
+            exit 1
+        fi
+    done
 
-trap 'tput cnorm; exit 0' SIGINT
+    trap 'tput cnorm; exit 0' SIGINT
 
-clear
-tput civis
+    clear
+    tput civis
 
-while :; do
-    tput cup 0 0
+    clock=$(render)
+    height=$(printf $clock | wc -l)
+    width=$(( $(printf $clock | head -1 | wc -m) - 1 ))
+
+    center_x=$(( ($COLUMNS / 2) - ($width  / 2) ))
+    center_y=$(( ($LINES   / 2) - ($height / 2) ))
+
+    centering_spaces=$(           awk 'BEGIN{ for(i = 0; i < '$(( ($center_y * $COLUMNS) ))'; i++) { printf " " } print; exit }')
+    horizontal_centering_spaces=$(awk 'BEGIN{ for(i = 0; i < '$center_x';                     i++) { printf " " } print; exit }')
+
+    while :; do
+        tput cup 0 0
+        printf "$centering_spaces"
+        render | sed 's/^/'"$horizontal_centering_spaces"'/'
+        sleep 1
+    done  
+
+    tput cnorm
+}
+
+render() {
     date +%X | toilet --metal -F border -f ascii12
-    sleep 1
-done  
+}
 
-tput cnorm
+main "$@"
