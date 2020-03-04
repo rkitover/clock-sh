faf443a11988023ca1f157c0e92403466b796b48
Rafael Kitover <rkitover@gmail.com>
2020-03-04 15:56:39 +0000

    Fix centering.
    Fix height calculation by quoting the variable.
    
    Use ceiling for integer division.
    
    Remove ansi codes when calculating clock size.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- clock.sh
+++ clock.sh
@@ -3,28 +3,32 @@
 main() {
     for req in tput toilet; do
         if ! command -v $req >/dev/null; then
-            printf >&2 "ERROR: please install $req\n"
+            printf >&2 "please install $req\n"
             exit 1
         fi
     done
 
     trap 'tput cnorm; exit' SIGINT
 
+    LINES=$(  tput lines)
+    COLUMNS=$(tput cols)
+
+    clock=$(render)
+    height=$(printf "$clock" | wc -l)
+    width=$(( $(printf "$clock" | head -1 | wc -m) - 1 ))
+
+    # +1 for ceiling
+    center_x=$(( (($COLUMNS + 1) / 2) - (($width  + 1) / 2) ))
+    center_y=$(( (($LINES   + 1) / 2) - (($height + 1) / 2) ))
+
+    horizontal_centering_spaces=$(dup $(( $center_x - 1 )) ' ')
+
     clear
     tput civis
 
-    clock=$(render)
-    height=$(printf $clock | wc -l)
-    width=$(( $(printf $clock | head -1 | wc -m) - 1 ))
-
-    center_x=$(( ($COLUMNS / 2) - ($width  / 2) ))
-    center_y=$(( ($LINES   / 2) - ($height / 2) ))
-
-    horizontal_centering_spaces=$(dup $center_x ' ')
-
     while :; do
-        tput cup $center_y 0
-        render | sed 's/^/'"$horizontal_centering_spaces"'/'
+        tput cup $(( $center_y - 1 )) 0
+        render --metal | sed 's/^/'"$horizontal_centering_spaces"'/'
         # Sleep until next second.
         sleep $(printf "scale=3; 1 - $(date +%N) / 1000000000\n" | bc)
     done  
@@ -33,7 +37,7 @@ main() {
 }
 
 render() {
-    date +%X | toilet --metal -F border -f ascii12
+    date +%X | toilet -F border -f ascii12 "$@"
 }
 
 dup() {
